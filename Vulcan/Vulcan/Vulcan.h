b3b08631c9f5374ba9d0829d428daeb6d9023892
Jin Sasaki <aysm47@gmail.com>
2016-10-28 17:19:03 +0900

    Support Carthage

---
+++ Vulcan/Vulcan/Vulcan.h
@@ -0,0 +1,19 @@
+//
+//  Vulcan.h
+//  Vulcan
+//
+//  Created by Jin Sasaki on 2016/10/28.
+//  Copyright © 2016年 Sasakky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Vulcan.
+FOUNDATION_EXPORT double VulcanVersionNumber;
+
+//! Project version string for Vulcan.
+FOUNDATION_EXPORT const unsigned char VulcanVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Vulcan/PublicHeader.h>
+
+
